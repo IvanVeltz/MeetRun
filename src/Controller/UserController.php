cb02a43a859598fca234ae755b444fe07db5dad7
Ivan Veltz <ivan.veltz@live.fr>
2025-06-02 16:32:04 +0200

    changement du profil en bdd, manque la gestion de la photo

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ProfilForm;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    public function index(Request $request,Security $security, EntityManagerInterface $entityManager): Response
     {
+        $user = $security->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_login'); // Redirige si aucun utilisateur connecté
+        }
+
+        $form = $this->createForm(ProfilForm::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'profilForm' => $form,
         ]);
     }
 }
--- src/Form/ProfilForm.php
+++ src/Form/ProfilForm.php
@@ -3,21 +3,73 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ProfilForm extends AbstractType
 {
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security; // Stocke l'instance de Security
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $bioText = $this->security->getUser()->getBio() ? $this->security->getUser()->getBio() : "Parlez un peu de vous...";
+
         $builder
-            ->add('dateOfBirth')
-            ->add('pictureProfilUrl')
-            ->add('postalCode')
-            ->add('city')
-            ->add('bio')
+            ->add('dateOfBirth',DateType::class, [
+                'label' => 'Date de naissance',
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('pictureProfilUrl', FileType::class, [
+                'label' => 'Photo de profil',
+                'mapped' => false, // Ne lie pas directement le champ à l'entité (gestion manuelle)
+                'required' => false, // Rend le champ facultatif
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'mimeTypes' => ['image/jpeg', 'image/png'],
+                        'mimeTypesMessage' => 'Veuillez télécharger une image valide (JPEG ou PNG).',
+                    ]),
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('postalCode', TextType::class, [
+                'label' => 'Code Postal',
+                "attr" => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+                "attr" => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('bio', TextareaType::class, [
+                'label' => 'Presentation',
+                'attr' => [
+                    'class' => 'form-control',
+                    'rows' => 5, // Définit la hauteur du champ
+                    'placeholder' => $bioText,
+                ],
+            ])
         ;
     }
 
