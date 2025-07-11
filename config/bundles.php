842304e54465035dfc9ebf848ae6b52677173399
Ivan Veltz <ivan.veltz@live.fr>
2025-07-11 09:12:01 +0200

    install easyadmin

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,6 @@ return [
     Karser\Recaptcha3Bundle\KarserRecaptcha3Bundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
