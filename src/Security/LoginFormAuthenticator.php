da6f0dbd2a37d1b14d066dd616e52619b5a6ad32
Ivan Veltz <ivan.veltz@live.fr>
2025-09-08 16:58:35 +0200

    rate_limiter

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -31,11 +31,13 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
     public function authenticate(Request $request): Passport
     {
         $email = $request->request->get('email', '');
+        $ip = $request->getClientIP();
 
         // Limiter par email ou IP
-        $limiter = $this->loginLimiter->create($email);
-        $limit = $limiter->consume(1);
-        if (!$limit->isAccepted()) {
+        $key = $email.'-'.$ip; 
+        $limiter = $this->loginLimiter->create($key); // recupere le compteur pour la combinaison email - IP
+        $limit = $limiter->consume(1); // consomme une tentative
+        if (!$limit->isAccepted()) { // Indique si l'utilisateur peut encore essayer
             throw new CustomUserMessageAuthenticationException('Trop de tentatives. Réessayez plus tard.');
         }
 
