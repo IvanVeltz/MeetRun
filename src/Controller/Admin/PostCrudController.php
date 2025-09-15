67d6512a600e8da3ce4f8e5cb265a01444c78b33
Ivan Veltz <ivan.veltz@live.fr>
2025-09-15 14:48:42 +0200

    easy admin

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -3,16 +3,26 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Post;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PostCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Post::class;
+    }
+
+    public function configureFields(string $pageName): iterable{
+        return[
+            IntegerField::new('id', 'id'),
+            TextField::new('message', 'message'),
+            DateTimeField::new('createdAt', 'créé le')
+        ];
     }
 
     /*
